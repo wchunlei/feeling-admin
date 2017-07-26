7f2a3b9a3f8f1ae6fd0404122a18469484d7f2ea
fei.chen <fei.chen@192.168.4.227>
2017-07-26 15:22:16 +0800

    js

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,5 @@
 module.exports = {
     NODE_ENV: '"production"',
-    BASE_API: '"https://api-prod"',
+    BASE_API: '"http://192.168.1.43:40000"',
     APP_ORIGIN: '"https://wallstreetcn.com"'
 };
