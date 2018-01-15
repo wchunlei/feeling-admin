d3f3611a69c043c18d30646d5269a623fdd7bfc6
chunlei.wang <1027933381@qq.com>
2018-01-15 10:59:12 +0800

    modify

--- src/views/charts/mixchart.vue
+++ src/views/charts/mixchart.vue
@@ -1,4 +1,4 @@
-<template>
+<!--<template>
   <div class="components-container" style='height:100vh'>
     <div class='chart-container'>
       <mix-chart id='apple' height='100%' width='100%'></mix-chart>
@@ -21,5 +21,5 @@
   height: 90%;
   padding-bottom: 40px;
 }
-</style>
+</style>-->
 
