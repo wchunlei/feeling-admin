636b1200b4f3bdd640658e08edda546bd80f6441
chunlei.wang <1027933381@qq.com>
2018-03-24 16:20:39 +0800

    add

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,7 +1,6 @@
 module.exports = {
     NODE_ENV: '"production"',
     /*BASE_API: '"http://192.168.1.43:30001"',*/
-    //BASE_API: '"http://opapimaid.inveno.cn:1441"',
-    BASE_API: '"http:/111.230.181.40:1441"',
+    BASE_API: '"http://111.230.181.40:1441"',
     APP_ORIGIN: '"https://wallstreetcn.com"'
 };
